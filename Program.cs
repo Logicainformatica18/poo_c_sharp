fd78f3d541174532679b236c57becda9bd05125f
Anthony Cardenas <logicainformatica18@gmail.com>
2019-11-09 10:23:48 -0500

    constructor y getter con atributos privados

--- Program.cs
+++ Program.cs
@@ -6,30 +6,23 @@ namespace POO_C_ {
         // atributes
        private string id;
        private string nombre;
-
+        //constructor
+        public persona (string id_, string nombre_) {
+            this.id=id_;
+            this.nombre=nombre_;
+        }
         // SETTER Y GETTER
         public string getId() {
          return this.id; 
         }
-
-        public void setId(string value_) {
-            this.id = value_;
-        }
-
         public string getNombre() {
            return  this.nombre; 
-        }
-
-        public void setNombre(string value) {
-            this.nombre = value;
         }
 
     }
     class Program {
         static void Main (string[] args) {
-            var alumno = new persona ();
-            alumno.setId("1");
-            alumno.setNombre("juan");
+            var alumno = new persona("1","juan");
             Console.WriteLine(alumno.getId());
             Console.WriteLine(alumno.getNombre());
            
