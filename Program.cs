8a572d3d697decc20da743d2a079775eaa716b6b
Anthony Cardenas <logicainformatica18@gmail.com>
2019-11-08 23:44:32 -0500

    clase persona y contructor

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace POO_C_ {
+
+    class persona {
+        // atributes
+        public string id;
+        public string nombre;
+        //constructor
+        public persona (string id_,string nombre_) {
+            this.id=id_;
+            this.nombre = nombre_;
+        }
+    }
+    class Program {
+        static void Main (string[] args) {
+            var alumno = new persona ("1","juan");
+            Console.WriteLine ("El id es : " + alumno.id);
+            Console.WriteLine ("El nombre es : " + alumno.nombre);
+        }
+    }
+}
