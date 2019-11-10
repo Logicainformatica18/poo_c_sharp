585ec876c7716b8ea676859eb139d2487ad43560
Anthony Cardenas <logicainformatica18@gmail.com>
2019-11-09 23:54:07 -0500

    asociacion y herencia persona-alumno-zona

--- alumno.cs
+++ alumno.cs
@@ -1,12 +1,15 @@
+using System.Runtime.CompilerServices;
 namespace POO_C_ {
     class alumno : persona{
         // atributes
         private string cod_carnet;
+        public object zonal;
         //constructor
-        public alumno(string nombres_,string paterno_, string cod_carnet_) {
+        public alumno(string nombres_,string paterno_, string cod_carnet_,object zonal_) {
             base.nombres=nombres_;
             base.paterno = paterno_;
             this.cod_carnet=cod_carnet_;
+            this.zonal=zonal_;
         }
         //GETTER
         public string getCod_carnet() {
---
+++ zona.cs
@@ -0,0 +1,14 @@
+namespace POO_C_ {
+    public class zona {
+        public string nombre_zona;
+        
+        public  zona(string nombre_zona_){
+            this.nombre_zona=nombre_zona_;
+        }
+
+        public string getZonal(){
+        return this.nombre_zona;
+    }
+    }
+
+}
